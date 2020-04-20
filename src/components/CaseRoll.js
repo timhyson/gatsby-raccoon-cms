45c4753c63460b24339bb8b38e4f2eb32a0c2a62
Tim Hyson <tjhyson@gmail.com>
2020-04-20 16:12:47 +0100

    Show 3 most recent case studies

--- src/components/CaseRoll.js
+++ src/components/CaseRoll.js
@@ -11,42 +11,44 @@ class CaseRoll extends React.Component {
         return (
             <div className="columns">
                 {posts &&
-                    posts.map(({ node: post }) => (
-                        <div key={post.id} className="is-parent column">
-                            <article
-                                className={`case-study-item tile is-child box notification ${
-                                    post.frontmatter.featuredpost ? "is-featured" : ""
-                                }`}
-                            >
-                                <header>
-                                    {post.frontmatter.featuredimage ? (
-                                        <div className="featured-thumbnail">
-                                            <PreviewCompatibleImage
-                                                imageInfo={{
-                                                    image: post.frontmatter.featuredimage,
-                                                    alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                                }}
-                                            />
-                                        </div>
-                                    ) : null}
-                                </header>
-                                <p className="post-meta">
-                                    <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
-                                        {post.frontmatter.title}
-                                    </Link>
-                                </p>
-                                <p className="subtitle is-size-5 is-block">{post.frontmatter.description}</p>
-                                <p>
-                                    {/* {post.excerpt} */}
-                                    <br />
-                                    <br />
-                                    <Link className="button" to={post.fields.slug}>
-                                        Keep Reading →
-                                    </Link>
-                                </p>
-                            </article>
-                        </div>
-                    ))}
+                    posts
+                        .slice(0, 3) // show three most recent case studies
+                        .map(({ node: post }) => (
+                            <div key={post.id} className="is-parent column">
+                                <article
+                                    className={`case-study-item tile is-child box notification ${
+                                        post.frontmatter.featuredpost ? "is-featured" : ""
+                                    }`}
+                                >
+                                    <header>
+                                        {post.frontmatter.featuredimage ? (
+                                            <div className="featured-thumbnail">
+                                                <PreviewCompatibleImage
+                                                    imageInfo={{
+                                                        image: post.frontmatter.featuredimage,
+                                                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                                                    }}
+                                                />
+                                            </div>
+                                        ) : null}
+                                    </header>
+                                    <p className="post-meta">
+                                        <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                                            {post.frontmatter.title}
+                                        </Link>
+                                    </p>
+                                    <p className="subtitle is-size-5 is-block">{post.frontmatter.description}</p>
+                                    <p>
+                                        {/* {post.excerpt} */}
+                                        <br />
+                                        <br />
+                                        <Link className="button" to={post.fields.slug}>
+                                            Keep Reading →
+                                        </Link>
+                                    </p>
+                                </article>
+                            </div>
+                        ))}
             </div>
         )
     }
